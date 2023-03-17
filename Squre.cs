851a5e96ad017e885e5d596443ce09c3b4ca15d7
VivekPratapsingh202001 <43576137+VivekPratapsingh202001@users.noreply.github.com>
2023-03-17 15:34:07 +0530

    enter any number and print Square that number

---
+++ Squre.cs
@@ -0,0 +1,13 @@
+class Squre
+ {
+	 static void Main(string []args)
+	 {
+	   int squre=1;
+		 System.Console.Write("enter any number ");
+		int num=System.Convert.ToInt32(System.Console.ReadLine());
+		 squre=num*num;
+		 
+	    System.Console.Write("squre of number"+squre);
+	 }
+ }
+ 
